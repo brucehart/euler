b3f1d3d4723e88c86f412cfae1063c833c40bade
Bruce Hart <bruce.hart@gmail.com>
2012-05-28 10:40:24 -0400

    Completed problem 13

--- problem13/euler13.py
+++ problem13/euler13.py
@@ -1,20 +1,28 @@
 #Author: Bruce Hart <bruce.hart@gmail.com>
 
 #Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
-# (see problem13_data.txt)
+# (see problem13_d.txt)
 
-def sumCol(c):
-    s = sum(map(lambda x: int(x),c))
+def sumCol(r,carry):
+    s = sum(r) + carry
     return [s//10,s%10]
 
-data = open('problem13_data.txt').readlines()
-data = map(lambda x: x.strip(),data)
+d = open('problem13_data.txt').readlines()
+d = map(lambda x: list(x.strip()),d)
+d = map(lambda x: map(lambda y: int(y),x),d)
 
 N = 50
+strSum = ''
+carry = 0
+sumResult = 0
 
 for k in xrange(N):
-    r = map(lambda x: x[-1],data)
-    [carry,sum] = sumCol(r)
-    print "{} {}".format(carry,sum)
+    r = map(lambda x: x[-k-1],d)
+    [carry,sumResult] = sumCol(r,carry)
+    strSum = str(sumResult) + strSum
+
+strSum = str(carry) + strSum
+print strSum[0:10]
+
 
 
