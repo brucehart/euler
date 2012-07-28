513583da0c8012c7494ecb965b26e701359c7009
Bruce J. Hart <bruce.hart@gmail.com>
2012-07-27 23:26:07 -0400

    Clean up on problem 17

--- problem17/euler17.py
+++ problem17/euler17.py
@@ -9,7 +9,12 @@
 
 import time
 
-def numberToPhrase(n):
+def numberToPhraseLen(n):
+    """
+        numberToPhraseLen(n) - Computes the phrase length for a given number (for values up to 10,000)
+    """
+
+    #letter counts for zero to twenty, thirty, forty...ninety, hundred, and thousand
     letterCounts = [4, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8, 6, 6, 5, 5, 5, 7, 6, 6, 7, 8]
 
     if (n <= 20):
@@ -20,28 +25,26 @@ def numberToPhrase(n):
         else:
             return letterCounts[(n-20)//10+20] + letterCounts[n%10]
     elif (n<1000):
-        #letterCounts[29] --> len("hundred")
+        #letterCounts[28] --> len("hundred")
         if (n % 100 == 0):
             return letterCounts[n//100] + letterCounts[28]
         else:
-            return letterCounts[n//100] + letterCounts[28] + 3 + numberToPhrase(n%100)
-    elif (n < 10000):
+            #add 3 for the "and" after hundred
+            return letterCounts[n//100] + letterCounts[28] + 3 + numberToPhraseLen(n%100)
+    elif (n <= 10000):
         #letterCounts[29] --> len("thousand")
         if (n % 1000 == 0):
             return letterCounts[n//1000] + letterCounts[29]
         else:
-            return letterCounts[n//1000] + letterCounts[29] + numberToPhrase(n%1000)
+            return letterCounts[n//1000] + letterCounts[29] + numberToPhraseLen(n%1000)
     else:
         return 0
 
 
 def euler17():
-    #for i in range(1,1001):
-    #    print str(i) + ":" + str(numberToPhrase(i))
-
     N = 1000
     D = xrange(1,N+1)
-    print sum(map(numberToPhrase,D))
+    print sum(map(numberToPhraseLen,D))
 
 if __name__ == '__main__':
     time.clock()
