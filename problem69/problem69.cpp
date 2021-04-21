9fbfb185ae8402a1584dbf2cba891ae39f0fc44f
Bruce Hart <bruce.hart@gmail.com>
2021-04-21 11:44:07 -0400

    Completed problem 69.

--- problem69/problem69.cpp
+++ problem69/problem69.cpp
@@ -18,80 +18,43 @@
 
 #include <iostream>
 #include <map>
+#include <cmath>
 
 #define MAX_N   1000000
 
-unsigned int gcd(unsigned int u, unsigned int v)
+bool is_prime(int n)
 {
-    // Base cases
-    // gcd(n, n) = n
-    if (u == v)
-        return u;
-    
-    //  Identity 1: gcd(0, n) = gcd(n, 0) = n
-    if (u == 0)
-        return v;
-    if (v == 0)
-        return u;
+	for (int i=2;i<=sqrt(n);i++)
+	{
+		if (n%i == 0)
+			return false;
+	}
 
-    if (u & 1) { // u is odd
-        if (v % 2 == 0) // v is even
-            return gcd(u, v / 2); // Identity 3
-        // Identities 4 and 3 (u and v are odd, so u-v and v-u are known to be even)
-        if (u > v)
-            return gcd((u - v) / 2, v);
-        else
-            return gcd((v - u) / 2, u);
-    } else { // u is even
-        if (v & 1) // v is odd
-            return gcd(u / 2, v); // Identity 3
-        else // both u and v are even
-            return 2 * gcd(u / 2, v / 2); // Identity 2
+	return true;
+}
+
+int next_prime(int start=1)
+{
+    int x = start;
+
+    while(true)
+    {
+        if (is_prime(x)) return x;
+        x++;
     }
 }
 
 int main(int argc, char** argv)
 {
-    std::map<int, int> phi;
+    int m = 1;
+    int n = next_prime();
 
-    float max_ratio = 0.0001;
-    float target = 0.0;
-    int maxK = 0;
-
-    for (int i=2;i<=MAX_N;i++)
+    while (m*n <= MAX_N)
     {
-        phi[i] = 1;
-        target = i/max_ratio;
-
-        for (int j=2;j<i;j++)
-        {
-            if (gcd(j,i) == 1) phi[i]++;
-            if (phi[i] > target) break;
-        }
-
-        if (i % 1000 == 0) std::cout << i << std::endl;
-
-        if ((float)i/phi[i] > max_ratio) 
-        {
-            maxK = i;
-            max_ratio = (float)i/phi[i];
-        }
+        m = m*n;
+        n = next_prime(n+1);
     }
 
-    
-
-    for(int k=2; k<= MAX_N; k++)
-    {
-        //std::cout << k << " | " << phi[k] << " | " << (float)k/phi[k] << std::endl;
-        if (k/phi[k] > max_ratio)
-        {
-            max_ratio = k/phi[k];
-            maxK = k;
-        }
-    }
-
-    std::cout << maxK << std::endl;
-    std::cout << max_ratio << std::endl;
-
+    std::cout << m << std::endl;
     return 0;
 }
