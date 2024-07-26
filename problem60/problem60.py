605b34dba8484e1e86aa48ec79a4887e38515372
Bruce J. Hart <bruce.hart@gmail.com>
2024-07-26 10:48:28 -0400

    Removed sympy requirement from problem 60.

--- problem60/problem60.py
+++ problem60/problem60.py
@@ -1,16 +1,36 @@
 import itertools
-import sympy
+from math import sqrt, isqrt
 
-# Generate a list of prime numbers
+# Sieve of Eratosthenes to generate a list of prime numbers
 def generate_primes(limit):
-    primes = list(sympy.primerange(1, limit))
-    return primes
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    for start in range(2, isqrt(limit) + 1):
+        if sieve[start]:
+            for multiple in range(start*start, limit + 1, start):
+                sieve[multiple] = False
+    return [num for num, is_prime in enumerate(sieve) if is_prime]
+
+# Check if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 # Check if concatenating two primes results in another prime
 def is_prime_concatenation(prime1, prime2):
     concat1 = int(str(prime1) + str(prime2))
     concat2 = int(str(prime2) + str(prime1))
-    return sympy.isprime(concat1) and sympy.isprime(concat2)
+    return is_prime(concat1) and is_prime(concat2)
 
 # Recursively find sets of primes with the concatenation property
 def find_prime_sets(primes, set_size, current_set=[]):
