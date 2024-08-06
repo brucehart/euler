2987f9bc19980fca1590c29fd762a0b9c898d83f
Bruce J. Hart <bruce.hart@gmail.com>
2024-08-06 23:43:58 +0000

    Optimized problem 145.

--- problem145/problem145.cpp
+++ problem145/problem145.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
-
-using namespace std;
 
 // Function to reverse a number
 int reverseNumber(int n) {
@@ -25,16 +21,37 @@ bool allOddDigits(int n) {
     return true;
 }
 
+// Function to get the first digit of a number
+int firstDigit(int n) {
+    while (n >= 10) {
+        n /= 10;
+    }
+    return n;
+}
+
+// Function to get the last digit of a number
+int lastDigit(int n) {
+    return n % 10;
+}
+
 int main() {
     int count = 0;
     for (int n = 1; n < 1000000000; ++n) {
         if (n % 10 == 0) continue; // Skip numbers ending with 0 to avoid leading zeroes in reverse
+
+        int first = firstDigit(n);
+        int last = lastDigit(n);
+
+        // Skip numbers where the first and last digits are both even or both odd
+        if ((first % 2 == last % 2)) continue;
+
         int rev = reverseNumber(n);
         int sum = n + rev;
+
         if (allOddDigits(sum)) {
             ++count;
         }
     }
-    cout << count << endl;
+    std::cout << count << std::endl;
     return 0;
 }
