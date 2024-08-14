6941ba4c2025934c2956795bb2e92c58397a75d2
Bruce J. Hart <bruce.hart@gmail.com>
2024-08-14 17:22:32 -0400

    More Python 2 -> Python 3 conversion.

--- problem5/problem5.py
+++ problem5/problem5.py
@@ -1,42 +1,25 @@
-#Author: Bruce Hart <bruce.hart@gmail.com>
+# Author: Bruce Hart
+# Email: bruce.hart@gmail.com
 
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-#
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# Problem Description:
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-import time
-import itertools
+import math
 
+def lcm(a, b):
+    """Return the least common multiple of a and b."""
+    return abs(a*b) // math.gcd(a, b)
 
-def isDivisible(n,M):
-    for m in range(2,M+1):
-        if (n%m != 0):
-            return False
-    return True
-
-def euler5():
-    D = 20
-
-    n = xrange(2,D+1)
-    gMin = -1
-    match = []
-
-    #solution will be a combination of products from the range
-    #loop through these combinations only rather than an exhaustive list for dramatic speed improvement
-    for d in xrange(2,D+1):
-        #use itertools to easily define all combinations
-        #loop over xrange(2,D+1) to evaluate all combination list sizes
-        comb = itertools.combinations(n,d)
-
-        for c in comb:
-            #take the rolling product, if it is divisible by all terms and less than our min so far, record it
-            v = reduce(lambda x,y: x*y,c)
-            if ((gMin < 0 or v < gMin) and isDivisible(v,D) > 0):
-                gMin = v
-
-    print gMin
+def smallest_mult(n):
+    """Return the smallest number that is evenly divisible by all numbers from 1 to n."""
+    result = 1
+    for i in range(1, n + 1):
+        result = lcm(result, i)
+    return result
 
 if __name__ == '__main__':
-    time.clock()
-    euler5()
-    print "Executed in {} sec".format(time.clock())
+    # Calculate the smallest multiple for numbers 1 through 20
+    result = smallest_mult(20)
+    print(result)
+
--- problem6/problem6.py
+++ problem6/problem6.py
@@ -1,30 +1,35 @@
-#Author: Bruce Hart <bruce.hart@gmail.com>
+# Author: Bruce Hart
+# Email: bruce.hart@gmail.com
 
-#The sum of the squares of the first ten natural numbers is,
-#1^2 + 2^2 + ... + 10^2 = 385
+# Problem Description:
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
 #
-#The square of the sum of the first ten natural numbers is,
-#(1 + 2 + ... + 10)^2 = 55^2 = 3025
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
 #
-#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is 3025 - 385 = 2640.
 #
-#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
-import time
+# Task:
+# Find the difference between the sum of the squares of the first one hundred natural numbers
+# and the square of the sum.
 
 def euler6():
     D = 100
+    # In Python 3, range is used instead of xrange.
+    n = range(1, D+1)
 
-    n = xrange(1,D+1)
+    # Using a generator expression for sum of squares
+    sum_squares = sum(x**2 for x in n)
+    # Calculating the square of the sum
+    square_sum = sum(n) ** 2
 
-    sum_squares = reduce(lambda x,y: x+y**2,n)
-    square_sum = sum(n)**2
-
+    # Calculate the difference
     diff = square_sum - sum_squares
 
-    print diff
+    print(diff)
 
 if __name__ == '__main__':
-    time.clock()
     euler6()
-    print "Executed in {} sec".format(time.clock())
+
