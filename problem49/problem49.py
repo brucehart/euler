101e17ea888c0e02b586433d6572e7e37bc520f0
Bruce J. Hart <bruce.hart@gmail.com>
2024-08-15 10:40:19 -0400

    More Python2 -> Python3 conversion.

--- problem49/problem49.py
+++ problem49/problem49.py
@@ -1,74 +1,58 @@
 # -*- coding: utf-8 -*-
-#Author: Bruce Hart <bruce.hart@gmail.com>
+# Author: Bruce Hart
+# Email: bruce.hart@gmail.com
 
-#The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330,
-# is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the
-# 4-digit numbers are permutations of one another.
-#
-#There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property,
-# but there is one other 4-digit increasing sequence.
-#
-#What 12-digit number do you form by concatenating the three terms in this sequence?
+# This script finds unusual arithmetic sequences of three 4-digit primes,
+# where each term is a permutation of the others and the sequence has a constant difference.
+# The aim is to identify another sequence other than the known 1487, 4817, 8147, and concatenate them.
 
-import time
 import itertools
 
 def build_prime_list(N):
-    p = 2
-    last_p = p
-    prime_list = [1]*N
+    """ Generate a list indicating prime status for numbers up to N. """
+    prime_list = [True] * N
+    prime_list[0] = prime_list[1] = False
 
-    prime_list[0] = 0
-
-    while last_p < N:
-        p = 2*last_p
-
-        while p <= N:
-            prime_list[p-1] = 0
-            p = p + last_p
-
-        last_p = last_p + 1
-
-        while (last_p <= N) and (prime_list[last_p-1] == 0):
-            last_p = last_p + 1
+    for start in range(2, int(N**0.5) + 1):
+        if prime_list[start]:
+            for i in range(start*start, N, start):
+                prime_list[i] = False
 
     return prime_list
 
-
-def is_permutation(a,b):
-    if (a != b and sorted(list(str(a))) == sorted(list(str(b)))):
-        return True
-    else:
-        return False
+def is_permutation(a, b):
+    """ Check if two numbers are permutations of each other. """
+    return sorted(str(a)) == sorted(str(b))
 
 def euler49():
-    primes = build_prime_list(10**4+1)
+    """ Search for all 4-digit arithmetic sequences of primes that are permutations of each other. """
+    primes = build_prime_list(10000)
     results = []
-    skips = []
+    skips = set()
 
-    for i in range(1000, 9999):
-
-        if (i in skips):
+    for i in range(1000, 10000):
+        if i in skips:
             continue
 
-        p = filter(lambda x: is_permutation(i,x), range(1000,9999))
+        # Generate permutations that are also primes
+        permutations = [x for x in range(1000, 10000) if is_permutation(i, x) and primes[x]]
+        skips.update(permutations)
 
-        map(skips.append, p)
+        # Check the specific condition excluding the known sequence
+        if len(permutations) >= 3 and 1487 not in permutations:
+            permutations.sort()
+            for j in range(len(permutations)):
+                for k in range(j + 1, len(permutations)):
+                    diff = permutations[k] - permutations[j]
+                    if permutations[k] + diff in permutations:
+                        results.append((permutations[j], permutations[k], permutations[k] + diff))
 
-        p = filter(lambda x: (primes[x-1] == 1), p)
-
-        #find sets with at least 3 entries to evaluate
-        #and skip the example set given in the problem
-        if (len(p) >= 3) and (1487 not in p):
-            p = sorted(p)
-
-            for j in range(0, len(p)-1):
-                for k in range(j+1, len(p)):
-                    if (2*p[k] - p[j]) in p:
-                        print (p[j], p[k], 2*p[k] - p[j])
-
+    # Print results excluding the initial example
+    for result in results:
+        if 1487 not in result:
+            print("".join(str(x) for x in result))
+            break
 
 if __name__ == '__main__':
-    time.clock()
     euler49()
-    print "Executed in {0} sec".format(time.clock())
+
