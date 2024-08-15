f6d7a695e8656bf3e7c5f000c6b39d77b276748f
Bruce J. Hart <bruce.hart@gmail.com>
2024-08-15 10:40:19 -0400

    More Python2 -> Python3 conversion.

--- problem53/problem53.py
+++ problem53/problem53.py
@@ -1,32 +1,24 @@
-#Author: Bruce Hart <bruce.hart@gmail.com>
+# Author: Bruce Hart
+# Email: bruce.hart@gmail.com
 
-#There are exactly ten ways of selecting three from five, 12345:
-#
-#123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
-#
-#In combinatorics, we use the notation, 5C3 = 10.
-#
-#In general, nCr = n!/(r!(n-r)!)
-#where r = n, n! = n*(n-1)*...*3*2*1, and 0! = 1.
-#
-#It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
-#How many, not necessarily distinct, values of  nCr, for 1 = n = 100, are greater than one-million?
+# The code calculates the number of combinations nCr that exceed one million for 1 <= n <= 100.
+# nCr represents the number of ways to choose r elements from a set of n elements, calculated as n! / (r! * (n-r)!).
+# The challenge is to find how many combinations exceed one-million across different values of n.
 
-
-import time
 import math
 
-def nCr(n,r):
-    return math.factorial(n)/(math.factorial(r)*math.factorial(n-r))
+def nCr(n, r):
+    """ Calculate the binomial coefficient nCr = n! / (r! * (n-r)!) """
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
     
-def nCrCount(n,lim):
-    return len(filter(lambda x: x>lim,map(lambda r: nCr(n,r),xrange(1,n+1))))
+def nCrCount(n, lim):
+    """ Count how many values of nCr for a given n are greater than lim """
+    return sum(1 for r in range(1, n + 1) if nCr(n, r) > lim)
     
 def euler53():
-    print sum(map(lambda x: nCrCount(x,10**6),xrange(1,101)))
-    
+    """ Calculate the number of combinations greater than one million for 1 <= n <= 100 """
+    print(sum(nCrCount(n, 10**6) for n in range(1, 101)))
 
 if __name__ == '__main__':
-    time.clock()
     euler53()
-    print "Executed in {0} sec".format(time.clock())
+
